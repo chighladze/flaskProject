1d9babf22c0a75336c23e1299f3097d6cc9467b6
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-03-15 11:30:16 +0400

    add form and git ignore

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 
 
+def user_data():
+    user_ip = request.remote_addr
+
+    # Get request method
+    request_method = request.method
+
+    # Get user agent
+    user_agent = request.user_agent.string
+
+    # Get query parameters
+    query_params = request.args
+
+    # Get request headers
+    headers = request.headers
+
+    # Access cookies
+    cookies = request.cookies
+
+    # print(user_ip, request_method, user_agent, query_params, headers, cookies)
+
 @app.route('/')
 def hello_world():  # put application's code here
+
+    # get user data
+    user_data()
+
     return render_template('index.html')
 
 
